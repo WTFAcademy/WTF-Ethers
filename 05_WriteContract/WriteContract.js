f34dd8f5a001e497616c29ff6e22ea68881f29df
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-08-20 02:40:48 +0800

    Update WriteContract.js

--- 05_WriteContract/WriteContract.js
+++ 05_WriteContract/WriteContract.js
@@ -1,6 +1,6 @@
-// 声明只读合约的规则：
-// 参数分别为合约地址`address`，合约ABI `abi`，Provider变量`provider`
-// const contract = new ethers.Contract(`address`, `abi`, `provider`);
+// 声明只可写合约的规则：
+// const contract = new ethers.Contract(`address`, `abi`, `wallet`);
+// 参数分别为合约地址`address`，合约ABI `abi`，Wallet变量`wallet`
 
 import { ethers } from "ethers";
 // playcode免费版不能安装ethers，用这条命令，需要从网络上import包（把上面这行注释掉）
@@ -9,27 +9,64 @@ import { ethers } from "ethers";
 // 利用Infura的rpc节点连接以太坊网络
 // 准备Infura API Key, 教程：https://github.com/AmazingAng/WTFSolidity/blob/main/Topics/Tools/TOOL02_Infura/readme.md
 const INFURA_ID = '184d4c5ec78243c290d151d3f1a10f1d'
-// 连接以太坊主网
+// 连接Rinkeby测试网
 const provider = new ethers.providers.JsonRpcProvider(`https://rinkeby.infura.io/v3/${INFURA_ID}`)
 
+// 利用私钥和provider创建wallet对象
+const privateKey = '0x227dbb8586117d55284e26620bc76534dfbd2394be34cf4a09cb775d593b6f2b'
+const wallet = new ethers.Wallet(privateKey, provider)
 
 // 第2种输入abi的方式：输入程序需要用到的函数，逗号分隔，ethers会自动帮你转换成相应的abi
 // 人类可读abi，以ERC20合约为例
-const abiERC20 = [
-    "function increment() external view returns(uint)",
+const abiWETH = [
+    "function balanceOf(address) public view returns(uint)",
+    "function deposit() public payable",
+    "function transfer(address, uint) public returns (bool)",
 ];
-// 代理合约: 0x9e5654cc8e0a147e1Dc884F462195E2d0313c63D
-// Implementation合约: 0x748e00F7cD688a5Ab100F004Cb64F99b854cd72D
-const addressDAI = '0x9e5654cc8e0a147e1Dc884F462195E2d0313c63D' // DAI Contract
-const contractDAI = new ethers.Contract(addressDAI, abiERC20, provider)
 
+// WETH合约（Rinkeby测试网）
+const addressWETH = '0xc778417e063141139fce010982780140aa0cd5ab' // WETH Contract
+// 声明可写合约
+const contractWETH = new ethers.Contract(addressWETH, abiWETH, wallet)
+// 也可以声明一个只读合约，在用contract.connect(wallet)转换成可写合约。
 
 const main = async () => {
-    // 2. 读取DAI合约的链上信息（IERC20接口合约）
-    const number = await contractDAI.increment()
-    console.log("\n2. 读取Logic合约信息")
-    console.log(`合约地址: ${addressDAI}`)
-    console.log(`输出: ${number}`)
+    
+    const address = await wallet.getAddress()
+    // 1. 读取WETH合约的链上信息（WETH abi）
+    console.log("\n1. 读取WETH余额")
+    const balanceWETH = await contractWETH.balanceOf(address)
+    console.log(`存款前WETH持仓: ${ethers.utils.formatEther(balanceWETH)}\n`)
+    
+    // 如果钱包ETH足够
+    if(ethers.utils.formatEther(balanceWETH) > 0.0015){
+
+        // 2. 调用desposit函数，将0.001 ETH转为WETH
+        console.log("\n2. 调用desposit()函数，存入0.001 ETH")
+        // 发起交易
+        const tx = await contractWETH.deposit({value: ethers.utils.parseEther("0.001")})
+        // 等待交易上链
+        await tx.wait()
+        console.log(`交易详情：`)
+        console.log(tx)
+        const balanceWETH_deposit = await contractWETH.balanceOf(address)
+        console.log(`存款后WETH持仓: ${ethers.utils.formatEther(balanceWETH_deposit)}\n`)
+
+        // 3. 调用transfer()函数，将0.001 WETH转账给 vitalik
+        console.log("\n3. 调用transfer()函数，给vitalik转账0.001 WETH")
+        // 发起交易
+        const tx2 = await contractWETH.transfer("vitalik.eth", ethers.utils.parseEther("0.001"))
+        // 等待交易上链
+        await tx2.wait()
+        const balanceWETH_transfer = await contractWETH.balanceOf(address)
+        console.log(`转账后WETH持仓: ${ethers.utils.formatEther(balanceWETH_transfer)}\n`)
+
+    }else{
+        // 如果ETH不足
+        console.log("ETH不足，去水龙头领一些Rinkeby ETH")
+        console.log("1. chainlink水龙头: https://faucets.chain.link/rinkeby")
+        console.log("2. paradigm水龙头: https://faucet.paradigm.xyz/")
+    }
 }
 
 main()
