ee1898a495300f2e1f7d9b07192e7933b5e16ada
Li, Amazing Ang <d0u.gie@hotmail.com>
2022-09-05 01:55:11 +0800

    Create ERC721Check.js

---
+++ 12_ERC721Check/ERC721Check.js
@@ -0,0 +1,36 @@
+import { ethers } from "ethers";
+
+//准备 alchemy API 可以参考https://github.com/AmazingAng/WTFSolidity/blob/main/Topics/Tools/TOOL04_Alchemy/readme.md 
+const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
+const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_MAINNET_URL);
+
+// 合约abi
+const abiERC721 = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function supportsInterface(bytes4) public view returns(bool)",
+];
+// ERC721的合约地址，这里用的BAYC
+const addressBAYC = "0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d"
+// 创建ERC721合约实例
+const contractERC721 = new ethers.Contract(addressBAYC, abiERC721, provider)
+
+// ERC721接口的ERC165 identifier
+const selectorERC721 = "0x80ac58cd"
+
+const main = async () => {
+    // 1. 读取ERC721合约的链上信息
+    const nameERC721 = await contractERC721.name()
+    const symbolERC721 = await contractERC721.symbol()
+    console.log("\n1. 读取ERC721合约信息")
+    console.log(`合约地址: ${addressBAYC}`)
+    console.log(`名称: ${nameERC721}`)
+    console.log(`代号: ${symbolERC721}`)
+
+    // 2. 利用ERC165的supportsInterface，确定合约是否为ERC721标准
+    const isERC721 = await contractERC721.supportsInterface(selectorERC721)
+    console.log("\n2. 利用ERC165的supportsInterface，确定合约是否为ERC721标准")
+    console.log(`合约是否为ERC721标准: ${isERC721}`)
+}
+
+main()
