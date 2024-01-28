29da084c886a81ca2cb9f8cde82b9af57dbc48a1
Mosamorphing <121625762+Mosamorphing@users.noreply.github.com>
2024-01-28 15:30:05 +0100

    Update ReadContract.js

--- en/03_ReadContract/ReadContract.js
+++ en/03_ReadContract/ReadContract.js
@@ -1,53 +1,59 @@
 // Declaration of the rules for a read-only contract:
-// Parameters are the contract address `address`, contract ABI `abi`, Provider variable `provider`
-// const contract = new ethers.Contract(`address`, `abi`, `provider`);
+// Parameters are the contract address `address`, contract `ABI`, Provider variable `provider`
+// const contract = new ethers.Contract(`address`, `ABI`, `provider`);
 
-import { ethers } from "ethers";
+const { ethers } = require "ethers";
 
-// Connect to the Ethereum network using the Alchemy RPC node
-// Prepare alchemy API, refer to https://github.com/AmazingAng/WTFSolidity/blob/main/Topics/Tools/TOOL04_Alchemy/readme.md
-const ALCHEMY_MAINNET_URL = 'https://eth-mainnet.g.alchemy.com/v2/oKmOQKbneVkxgHZfibs-iFhIlIAl6HDN';
-const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_URL);
+// Connect to the Ethereum network using the Infura RPC node
+const INFURA_MAINNET_URL = 'https://mainnet.infura.io/v3/8b9750710d56460d940aeff47967c4ba';
+const provider = new ethers.JsonRpcProvider(INFURA_MAINNET_URL);
 
 // Method 1 of providing ABI input: Copy the entire ABI
 // ABI for WETH can be copied from here: https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code
-const abiWETH = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]';
-const addressWETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' // WETH Contract
-const contractWETH = new ethers.Contract(addressWETH, abiWETH, provider)
+const WETH_ABI = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]';
+const WETH_address = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2' // WETH Contract
+const WETH_contract = new ethers.Contract(WETH_address, WETH_ABI, provider)
 
 // Method 2 of providing ABI input: Input the functions that the program needs, separated by commas, ethers will automatically convert them into corresponding ABI
 // Human-readable ABI, using ERC20 contract as an example
-const abiERC20 = [
+const ERC20_ABI = [
     "function name() view returns (string)",
     "function symbol() view returns (string)",
     "function totalSupply() view returns (uint256)",
     "function balanceOf(address) view returns (uint)",
 ];
-const addressDAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contractDAI = new ethers.Contract(addressDAI, abiERC20, provider)
+const DAI_address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
+const DAI_contract = new ethers.Contract(DAI_address, ERC20_ABI, provider)
 
 
 const main = async () => {
-    // 1. Read the on-chain information of the WETH contract (WETH ABI)
-    const nameWETH = await contractWETH.name()
-const symbolWETH = await contractWETH.symbol()
-const totalSupplyWETH = await contractWETH.totalSupply()
-console.log("\n1. Read WETH contract information")
-console.log(`Contract address: ${addressWETH}`)
-console.log(`Name: ${nameWETH}`)
-console.log(`Symbol: ${symbolWETH}`)
-console.log(`Total supply: ${ethers.formatEther(totalSupplyWETH)}`)
-const balanceWETH = await contractWETH.balanceOf('vitalik.eth')
-console.log(`Vitalik's holdings: ${ethers.formatEther(balanceWETH)}\n`)
+    // 1. Reading on-chain information of the WETH contract (WETH_ABI)
+  const name = await WETH_contract.name();
+  const symbol = await WETH_contract.symbol();
+  const totalSupply = await WETH_contract.totalSupply();
 
-// 2. Read information of DAI contract on the chain (IERC20 interface contract)
-const nameDAI = await contractDAI.name()
-const symbolDAI = await contractDAI.symbol()
-const totalSupplyDAI = await contractDAI.totalSupply()
-console.log("\n2. Read DAI contract information")
-console.log(`Contract address: ${addressDAI}`)
-console.log(`Name: ${nameDAI}`)
-console.log(`Symbol: ${symbolDAI}`)
-console.log(`Total supply: ${ethers.formatEther(totalSupplyDAI)}`)
-const balanceDAI = await contractDAI.balanceOf('vitalik.eth')
-console.log(`Vitalik's holdings: ${ethers.formatEther(balanceDAI)}\n`)
+  console.log(`\nReading from ${WETH_address}\n`);
+  console.log(`Name: ${name}`);
+  console.log(`Symbol: ${symbol}`);
+  console.log(`Total Supply: ${ethers.formatEther(totalSupply)}`);
+
+  const balanceAddress = 'vitalik.eth';
+  const balance = await WETH_contract.balanceOf('vitalik.eth');
+  console.log(`Vitalik's Balance: ${ethers.formatEther(balance)}\n`);
+
+  // 2. Reading on-chain information of the DAI contract (ERC20 interface contract)
+  const name = await DAI_contract.name();
+  const symbol = await DAI_contract.symbol();
+  const totalSupply = await DAI_contract.totalSupply();
+
+  console.log(`\nReading from ${DAI_address}\n`);
+  console.log(`Name: ${name}`);
+  console.log(`Symbol: ${symbol}`);
+  console.log(`Total Supply: ${ethers.formatEther(totalSupply)}`);
+
+  const balanceAddress = 'vitalik.eth';
+  const balance = await DAI_contract.balanceOf('vitalik.eth');
+  console.log(`Vitalik's Balance: ${ethers.formatEther(balance)}\n`);
+};
+
+main()
