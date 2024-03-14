770e489894804ddb23016de8f556a8707a446569
murphyyue <murphyyue@icloud.com>
2024-03-14 11:12:04 +0800

    fix(01_HelloVitalik): 修复en中的ethers引入方式以及增加注释

--- en/01_HelloVitalik/HelloVitalik.js
+++ en/01_HelloVitalik/HelloVitalik.js
@@ -1,10 +1,11 @@
 // Import the ethers package
-const { ethers } = require("ethers");
+import { ethers } from "ethers";
 // playcode's free version cannot install ethers, use this command to import the package from the internet (comment out the line above)
 // import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.2.3/ethers.js";
 
 // Connect to the Ethereum network using any desired provider (e.g INFURA or ALCHEMY) 
 // const provider = new ethers.JsonRpcProvider();
+// Note: if the current URL cannot be used, you need to register at NFURA or ALCHEMY's official website and get URL by yourself.
 const provider = new ethers.JsonRpcProvider(`https://mainnet.infura.io/v3/8b9750710d56460d940aeff47967c4ba`)
 // Specify the address you want to query
 const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045' //or 'vitalik.eth'
